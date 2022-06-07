65959b21b7f34cf32937aec87977b3bf9f386d38
Horuspokus <42516174+Horuspokus@users.noreply.github.com>
2022-06-07 15:18:39 +0300

    comm

---
+++ calc.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+exp = ""
+
+def press(num):
+     
+     global exp
+
+     exp = exp + str(num)
+
+     equation.set(exp)
+
+def equalpress():
+    #will figure it out
+    
